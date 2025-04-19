6dc3d09648b12debb235f1a7cd45e5e2587c9d68
Aemon33 <khazriaymen93@gmail.com>
2025-04-20 01:39:35 +0200

    add enum

--- Backend-dotnet/Program.cs
+++ Backend-dotnet/Program.cs
@@ -1,11 +1,19 @@
 using Backend_dotnet.Core.DbContext;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    }
+
+    );
 
 
 //DB
