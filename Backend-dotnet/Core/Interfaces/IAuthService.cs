ff3e3350a1005d5da755ebc93883de024c633400
Aemon33 <khazriaymen93@gmail.com>
2025-04-20 15:43:35 +0200

    add interfaces

---
+++ Backend-dotnet/Core/Interfaces/IAuthService.cs
@@ -0,0 +1,22 @@
+﻿using Backend_dotnet.Core.Dtos.Auth;
+using Backend_dotnet.Core.Dtos.General;
+using System.Security.Claims;
+
+namespace Backend_dotnet.Core.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<GeneralServiceResponseDto> SeedRolesAsync();
+        Task<GeneralServiceResponseDto> RegisterAsync(RegisterDto registerDto);
+        Task<LoginServiceResponseDto?> LoginAsync(LoginDto loginDto);
+        Task<GeneralServiceResponseDto> UpdateRoleAsync(ClaimsPrincipal User, UpdateRoleDto updateRoleDto);
+        Task<LoginServiceResponseDto?> MeAsync(MeDto meDto);
+        Task<IEnumerable<UserInfoResult>> GetUsersListAsync();
+        Task<UserInfoResult?> GetUserDetailsByUserNameAsync(string userName);
+        Task<IEnumerable<string>> GetUsernamesListAsync();
+
+
+
+
+    }
+}
