dd6845e951eab08f1eb9571ca7b69a07fe359390
Aemon33 <khazriaymen93@gmail.com>
2025-05-03 19:03:51 +0200

    add the DatabaseRecords.sql file

--- Backend-dotnet/obj/Debug/net8.0/Backend-dotnet.AssemblyInfo.cs
+++ Backend-dotnet/obj/Debug/net8.0/Backend-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c329bff859e895febe8cba08f617fd8b3893cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c63ff49757449d28d083e1e5c1db919bbba01c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
